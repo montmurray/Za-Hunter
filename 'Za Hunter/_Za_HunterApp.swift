e3405ed88c838f9c5cccfa2cfa72d7af300f7848
Tessa Murray <tmurray@bsd220.org>
2025-02-18 07:30:40 -0800

    Initial Commit

---
+++ 'Za Hunter/_Za_HunterApp.swift
@@ -0,0 +1,17 @@
+//
+//  _Za_HunterApp.swift
+//  'Za Hunter
+//
+//  Created by Tessa Murray on 2/18/25.
+//
+
+import SwiftUI
+
+@main
+struct _Za_HunterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
